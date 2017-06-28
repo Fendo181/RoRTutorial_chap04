7eff0a939cf23bbf422c7de25ba6cfd99f0401eb
endu <futoshi.endo@pepabo.com>
2017-06-28 18:41:36 +0900

    userモデルのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,18 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name:"Example user",email:"user@example.com")
+  end
+
+  #userオブジェクトが有効かどうか確認する。
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  #値存在性を確認する。
+  test "name should be present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
 end
