4565f8b9018d78bdf0b93b85f3d7eda0aa1df47d
endu <futoshi.endo@pepabo.com>
2017-06-28 15:30:19 +0900

    Userコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+
   root 'static_pages#home'
 
+  get  '/signup',  to: 'users#new' as:signup_path
   get  '/help',to:'static_pages#help'
   get  '/about',to: 'static_pages#about'
   get  '/contact',to: 'static_pages#contact'
