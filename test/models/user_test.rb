d424aeaa4b8275a2edea0da8e8b5f3ba3bd0abfd
endu <futoshi.endo@pepabo.com>
2017-06-29 15:58:03 +0900

    正規表現によるメールフォーマットのバリデーションを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,7 +25,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "naem should not be too long" do
+  test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
   end
@@ -35,4 +35,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  #有効なメールフォーマットかどうかを確認するテスト
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?,"#{valid_address.inspect} should be vaild"
+    end
+  end
+
+  #無効なメールフォーマットをテストする。
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?,"#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
