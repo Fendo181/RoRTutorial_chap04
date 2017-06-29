e75e04703df4fcc93f5555b0d789e1bd0789c0b2
endu <futoshi.endo@pepabo.com>
2017-06-29 15:01:40 +0900

    fix user.rb user_test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  #name属性の存在を検証する。
   validates :name, presence:true
   validates :email, presence:true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,15 +8,18 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name:"Example user",email:"user@example.com")
   end
 
+  # trueであれば成功
   test "should be valid" do
     assert @user.valid?
   end
 
+  #nameが空なら無効としたい = 無効(false)で成功ならassert_notメソッドを使う。
   test "name should be present" do
     @user.name = " 　"
     assert_not @user.valid?
   end
 
+  #emailが空なら無効としたい = 無効(flase)で成功ならassert_notメソッドを使う。
   test "email should be present" do
     @user.email = "  "
     assert_not @user.valid?
