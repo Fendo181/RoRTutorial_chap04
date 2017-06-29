2fc7f379d54d0179a3336ef285cc8e96da7eb9df
endu <futoshi.endo@pepabo.com>
2017-06-29 16:33:46 +0900

    メールアドレスの小文字化に対するバリデーションとテストを追加した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save{self.email = self.email.downcase}
+
   validates :name, presence:true
   validates :email, presence:true
   validates :name, presence:true, length: { maximum:50}
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -63,4 +63,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  #メールアドレス小文字になっているかの確認テスト
+  test "email addresses should be saved as lower-case " do
+     mixed_case_email = "Foo@ExAMPle.CoM"
+     @user.email = mixed_case_email
+     @user.save
+     assert_equal mixed_case_email.downcase,@user.reload.email
+  end
+
 end
