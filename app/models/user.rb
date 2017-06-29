4d05fa585caba3b8936335f1e33d45c92dc29fb2
endu <futoshi.endo@pepabo.com>
2017-06-29 14:19:30 +0900

    Update usermodel test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  #name属性の存在を検証する。
+  validates :name, presence:true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,14 +8,13 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name:"Example user",email:"user@example.com")
   end
 
-  #userオブジェクトが有効かどうか確認する。
   test "should be valid" do
     assert @user.valid?
   end
 
-  #値存在性を確認する。
   test "name should be present" do
     @user.name = "  "
     assert_not @user.valid?
   end
+
 end
