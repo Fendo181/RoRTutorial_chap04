2ff1d0bff2fd0c6037f01fdf7ea0d0c9d560b65e
endu <futoshi.endo@pepabo.com>
2017-07-04 14:38:12 +0900

    ログイン失敗時のフラッシュメッセージを表示するよう処理を追加した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         # ユーザログイン後にユーザ情報にリダイレクトする。
       else
-        # エラーメッセージを作成する。
+        flash[:danger] = "Invalid email/password combination"
         render 'new'
       end
   end
