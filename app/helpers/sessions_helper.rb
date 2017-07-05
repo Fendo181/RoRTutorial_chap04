ef2773c1996224d060161131a5fd46ba0a87f3d4
endu <futoshi.endo@pepabo.com>
2017-07-05 14:39:37 +0900

    ユーザログアウト処理を実装しました。

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #現在のユーザでログアウトする。
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil  
+  end
+
   #現在ログイン中のユーザを返す(いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
