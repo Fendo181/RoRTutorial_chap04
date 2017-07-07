2d9d87c95cb0cfbe63af5fc13d200134279bafd7
endu <futoshi.endo@pepabo.com>
2017-07-07 15:47:20 +0900

    テストが遠るように、logged_in?でtrueの場合のみlogoutするよう記述した。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    #logged_in?がされている場合のみlog_outする。
+    log_out if logged_in?
     redirect_to root_url
   end
 end
