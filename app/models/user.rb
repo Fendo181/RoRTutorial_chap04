737246e56f02af52223b95103f09da48e00db15a
endu <futoshi.endo@pepabo.com>
2017-07-07 14:03:39 +0900

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save{email.downcase!}
+  attr_accessor :remember_token
 
   validates :name, presence: true
   validates :email, presence: true
@@ -17,6 +18,18 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string,cost: cost)
+  end
 
+  #ランダムなトークンを渡す。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #永続セッションのためにユーザーをデータベースに記憶する。
+  def remember
+    #記憶トークンを作成
+    self.remember_token = User.new_token
+    #記憶ダイジェストを更新
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
