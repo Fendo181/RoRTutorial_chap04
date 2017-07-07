7bc6a03fa560189596b42d2b34fe23d0a023e3c9
endu <futoshi.endo@pepabo.com>
2017-07-07 14:17:44 +0900

    session_helperにユーザを永続的に記憶するrememberメソッドを追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  #現在のユーザでログアウトする。
+  #ユーザを永続的にセッションに記憶する。
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  #ユーザーを永続的セッションに記憶する
   def log_out
     session.delete(:user_id)
     @current_user = nil
