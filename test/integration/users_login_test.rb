cc4003d474536e6e8f19a599dffb0744c72c32f0
endu <futoshi.endo@pepabo.com>
2017-07-07 15:37:53 +0900

    ログアウトのテストコードに2番目のウィンドウでログアウトをクリックするシュミレートするコードを追加する。

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,7 +18,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   #fixtureのデータを使ったユーザログイン/ログアウトのテスト
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
@@ -31,6 +31,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    #2番目のウィンドウでログアウトをクリックするユーザをシミュレートする。
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
