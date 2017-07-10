656a98326b8e46c8f16a17e067460914275fec4f
endu <futoshi.endo@pepabo.com>
2017-07-10 15:25:23 +0900

    Add Test EditSuccess Code

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,4 +17,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
     assert_select "div.alert", 'The form contains 4 errors'
   end
+
+  #値を入力してeditが成功した事を確認するテスト
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    #データベースから最新のユーザー情報を読み込み直して、正しく更新されたかどうかを確認する。
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
