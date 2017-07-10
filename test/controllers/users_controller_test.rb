41d6cee2628523f1e78c4d0d5116ee358e220759
endu <futoshi.endo@pepabo.com>
2017-07-10 16:17:52 +0900

    Add EditTestCode to users_controller_test
    fixtureで新しいユーザを追加して、他のユーザが別のログイン済みのユーザページをeditした際のテストを追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,27 @@
 require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_users = users(:archer)
+  end
+
+  test 'should redirect edit whrn logged in as wrong user' do
+    #archerでログインする。
+    log_in_as(@other_user)
+    #なぜかmichaelのページをいじる。
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect edit when logged in as wrong user' do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
   test 'should get new' do
     get signup_path
     assert_response :success
