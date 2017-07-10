537d9366824a0ead318036c67a15c97afaedf7cd
endu <futoshi.endo@pepabo.com>
2017-07-10 14:52:50 +0900

    FIx validate
    nameに対するバリデーションが余計にあったので消しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,13 @@
 class User < ApplicationRecord
-  before_save{email.downcase!}
   attr_accessor :remember_token
-
-  validates :name, presence: true
-  validates :email, presence: true
+  before_save{email.downcase!}
   validates :name, presence:true, length: { maximum: 50 }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
+             format: { with: VALID_EMAIL_REGEX },
+             uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence:true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   # 渡された文字列のハッシュ値を渡す。
   def User.digest(string)
