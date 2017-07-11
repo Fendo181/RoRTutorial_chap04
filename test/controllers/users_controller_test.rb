3df63fe8f2697fcb10f916af03a252204c957ea6
endu <futoshi.endo@pepabo.com>
2017-07-11 17:35:08 +0900

    Fix typo
    othe_usersになっていたので、other_userに変更

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
-    @other_users = users(:archer)
+    @other_user = users(:archer)
   end
 
   #ログインしてない状態でindexアクションにアクセスするとリダイレクトするを確認するテスト
@@ -13,23 +13,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   #adminの属性の変更が禁止されている事をテストする。
   test 'should not allow the admin attribute to be edited via the web' do
-    log_in_as(@other_users)
-    assert_not @other_users.admin?
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
     #adminの属性を変更する。
-    patch user_path(@other_users), params: {
-                                  user: { password: @other_users.password,
-                                          password_confirmation: @other_users.password,
+    patch user_path(@other_user), params: {
+                                  user: { password: @other_user.password,
+                                          password_confirmation: @other_user.password,
                                           admin: true
                                         }
     }
     #データベースを更新して確認する。
-    #@other_users.reload.admin?でfalseが返ってくるという事は変更されてない事を意味する。
-    assert_not @other_users.reload.admin?
+    #@other_user.reload.admin?でfalseが返ってくるという事は変更されてない事を意味する。
+    assert_not @other_user.reload.admin?
   end
 
   test 'should redirect edit whrn logged in as wrong user' do
     #archerでログインする。
-    log_in_as(@other_users)
+    log_in_as(@other_user)
     #なぜかmichaelのページをいじる。
     get edit_user_path(@user)
     assert flash.empty?
@@ -37,7 +37,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect edit when logged in as wrong user' do
-    log_in_as(@other_users)
+    log_in_as(@other_user)
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
