0df9d984d8507e9de0ad7422eb28bf494d469e93
endu <futoshi.endo@pepabo.com>
2017-07-14 15:47:17 +0900

    Fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,8 @@ class SessionsController < ApplicationController
         if user.activated?
           #ユーザログイン後にユーザ情報にリダイレクトする。
           log_in user
-          params[:session][:remember_token] == '1'? remember(user):forget(user)
-          redirect_back_oruser
+          params[:session][:remember_me] == '1'? remember(user):forget(user)
+          redirect_back_or user
         else
           message = "Account not activated."
           message += "Check your email for the activation link."
