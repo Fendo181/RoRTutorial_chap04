3b4a3ad2a668fd62a8c4dea847115e6de27b9f6f
endu <futoshi.endo@pepabo.com>
2017-07-20 17:44:26 +0900

    jbuilderでView側でJSONデータを返すようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,9 @@ class UsersController < ApplicationController
 
   def show
      @user = User.find(params[:id])
-     data = { id: @user.id,
-              name: @user.name,
-              email: @user.email,
-            }
      respond_to do |format|
        format.html { redirect_to root_url and return unless @user.activated? }
-       format.json { render json: data }
+       format.json
      end
    end
 
