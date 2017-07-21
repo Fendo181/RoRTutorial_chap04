c104b259261160f566b1991c36782e32eddc195b
endu <futoshi.endo@pepabo.com>
2017-07-21 19:04:14 +0900

    Add Jwt

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,5 @@
+require 'jwt'
+
 class AuthController < ApplicationController
 
   protect_from_forgery with: :null_session
@@ -5,6 +7,11 @@ class AuthController < ApplicationController
   def login_api
     @user = User.find_by(email: params[:email].downcase)
       if @user && @user.authenticate(params[:password])
+        #jwt
+        issued_at = Time.now
+        token = JWT.encode({ id: @user.id, iat: issued_at.to_i },
+                Rails.application.secrets.secret_key_base, 'HS256')
+        
         render "authed"
       else
         render "unauthed"
